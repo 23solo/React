8b939e042f3c27536c0ce276ddccc913e4c3e4fb
Abhishek Solanki <yagami.naruto23@gmail.com>
2021-02-03 17:56:35 +0530

    Component part 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="dark">
         <div className="container">
           <NavbarBrand href ="/" >Solo</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
